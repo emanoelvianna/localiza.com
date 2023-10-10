0b12d7fb91425d8ddcbec36bb0bca46aeaae64b5
emanoel_fabiano <emanoel_fabiano@sicredi.com.br>
2023-10-09 21:53:17 -0300

    dev - modificações iniciais

---
+++ src/carro-model.js
@@ -0,0 +1,6 @@
+class Carro {
+    constructor(placa) {
+        this.placa = placa;
+        // TODO: ADICIONAR OS CAMPOS PARA REPRESENTAR O CARRO
+    }
+}
---
+++ src/localiza.js
@@ -0,0 +1,11 @@
+var carro;
+
+function criarCarro() {
+    carro = new Carro("ABC123"); // TODO: COMO PEGAR O VALOR QUE O USUARIO ME INFORMA NO INPUT
+    console.log(carro);
+}
+
+function apresentarCarros() {
+    console.log(carro);
+}
+
