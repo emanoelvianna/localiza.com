d9272103933ae7c712127c23e2be64cf791a6ee4
emanoel_fabiano <emanoel_fabiano@sicredi.com.br>
2023-11-10 21:48:18 -0300

    dev - continuação do desenvolvimento

--- src/carro-model.js
+++ src/carro-model.js
@@ -1,6 +1,24 @@
 class Carro {
-    constructor(placa) {
+    constructor(placa, ano, cor, modelo, quilometragem, vDiaria, observacao) {
         this.placa = placa;
-        // TODO: ADICIONAR OS CAMPOS PARA REPRESENTAR O CARRO
+        this.ano = ano;
+        this.cor = cor;
+        this.modelo = modelo;
+        this.quilometragem = quilometragem;
+        this.vDiaria = vDiaria;
+        this.observacao = observacao;
     }
+
+    getPlaca() {
+        return this.placa;
+    }
+
+    getModelo() {
+        return this.modelo;
+    }
+
+    getAno() {
+        return this.ano;
+    }
+
 }
--- src/localiza.js
+++ src/localiza.js
@@ -1,12 +1,34 @@
-var carro;
+var carros = [];
 
 function criarCarro() {
     let placa = document.getElementById("placa-carro");
-    carro = new Carro(placa.value);
-    console.log(carro);
+    let ano = document.getElementById("ano-carro");
+    let cor = document.getElementById("cor-carro");
+    let modelo = document.getElementById("modelo-carro");
+    let quilometragem = document.getElementById("quilometragem-carro");
+    let vDiaria = document.getElementById("vDiaria-carro");
+    let observacao = document.getElementById("observacao-carro");
+
+    let carro = new Carro(placa.value, ano.value, cor.value, modelo.value, quilometragem.value, vDiaria.value, observacao.value);
+    
+    carros.push(carro);
+    limparCampos();
+    alert("Carro cadastrado!");
+}
+
+function limparCampos() {
+    document.getElementById("form-cadastro-carro").reset();
 }
 
 function apresentarCarros() {
-    console.log(carro);
+    let tabela = document.getElementById("lista-carros-cadastrados");
+    var linha = tabela.insertRow(1);
+    var placa = linha.insertCell(0);
+    var modelo = linha.insertCell(1);
+    var ano = linha.insertCell(2);
+
+    placa.innerHTML = carros[0].getPlaca();
+    modelo.innerHTML = carros[0].getModelo();
+    ano.innerHTML = carros[0].getAno();
 }
 
